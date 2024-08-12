5002179578d52e029359eb9c1103aec327944b50
IsaiMendezGarcia <isaindie2804@hotmail.com>
2024-08-11 18:31:22 -0600

    creacion archivo js

---
+++ script.js
@@ -0,0 +1,5 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+function
+
+let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o","ober"],["u", "ufat"]]
