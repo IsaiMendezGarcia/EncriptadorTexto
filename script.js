818e0243530f16c71e5a0cad9ffbafa72bedaa3b
IsaiMendezGarcia <isaindie2804@hotmail.com>
2024-08-11 19:01:03 -0600

    encriptado hecho

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
 
 function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o","ober"],["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
 
-    let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o","ober"],["u", "ufat"]]
-    stringEncriptada=stringEncriptada.tolowerCase()
-
-    for(let i = 0; i<matrizCodigo.length; i++){
+    for(let i = 0; i < matrizCodigo.length; i++){
         if(stringEncriptada.includes(matrizCodigo[i][0])){
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
     }
-    return stringEncriptada
+    return stringEncriptada;
 }
 
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+}
