9425b19f6c755b98d2d2cf19f74564f392d44671
IsaiMendezGarcia <isaindie2804@hotmail.com>
2024-08-11 18:47:18 -0600

    funcion encriptar

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
-function
 
-let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o","ober"],["u", "ufat"]]
+function encriptar(stringEncriptada){
+
+    let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o","ober"],["u", "ufat"]]
+    stringEncriptada=stringEncriptada.tolowerCase()
+
+    for(let i = 0; i<matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+}
+
